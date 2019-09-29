e932e183e3e1eb5ca49dbba8a7e5fd9c552a0345
Nicole M Darby <nicoledarby021322@gmail.com>
2019-09-29 22:58:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,14 +10,11 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
+      :patriarch => {name:"Lord Capulet", age:"50"
         },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {name:"Lady Capulet", age:"51"
         },
-      :heroine => {
-        #your key/value pairs here
+      :heroine => {name:"Juliet", age:"15", status:"alive"
         },
       :heroine_friends => []
    }
